72518fb8ad70265eddcf84c7ad3c8f14ef751927
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2020-08-28 22:49:06 -0300

    creating global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      outline: 0;
+  }
+
+  body {
+      background: #fff;
+      font-family: 'Lato', sans-serif;
+      -webkit-font-smoothing: antialiased;
+  }
+
+  h1, h2, h3, h4, h5, h6, input, button, strong {
+      font-family: 'Abel', sans-serif;
+      font-weight: 300;
+  }
+
+  h1 {
+      font-size: 40px;
+  }
+
+  button {
+      cursor: pointer;
+  }
+`;
