9682a7b1966a14afbbc0d4d81eae7f8102011f2f
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2020-08-28 22:49:25 -0300

    creating Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Header: React.FC = () => (
+  <Container>
+    <h1>Movies</h1>
+  </Container>
+);
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #145685;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  color: #17e4e1;
+`;
