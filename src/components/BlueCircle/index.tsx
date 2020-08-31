44891ac7b7a3b325f202df8692743bbb51bff94c
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2020-08-30 23:28:19 -0300

    passing parameters to BlueCircle

--- src/components/BlueCircle/index.tsx
+++ src/components/BlueCircle/index.tsx
@@ -3,13 +3,14 @@ import React, { HTMLAttributes } from "react";
 import { Container, InnerContainer } from "./styles";
 
 interface IBlueCircleProps extends HTMLAttributes<HTMLDivElement> {
-    circleSize: number;
+    circleSize?: number;
+    fontSize?: number;
 }
 
-const BlueCircle: React.FC<IBlueCircleProps> = ({circleSize, children, ...resProps}) => {
+const BlueCircle: React.FC<IBlueCircleProps> = ({circleSize, fontSize, children, ...resProps}) => {
   return (
-    <Container circleSize={circleSize} >
-      <InnerContainer>
+    <Container circleSize={circleSize}>
+      <InnerContainer circleSize={circleSize} fontSize={fontSize}>
         <h2>{children}</h2>
       </InnerContainer>
     </Container>
--- src/components/BlueCircle/styles.ts
+++ src/components/BlueCircle/styles.ts
@@ -1,20 +1,28 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 interface ICircleProps {
-    circleSize: number;
-  }
+  circleSize?: number;
+  fontSize?: number;
+}
 
 export const Container = styled.div<ICircleProps>`
   background: #145685;
-  height: ${(props) => props.circleSize}px;
-  width: ${(props) => props.circleSize}px;
+  height: 72px;
+  width: 72px;
   padding: 4px;
   border-radius: 50%;
   color: #17e4e1;
   position: relative;
+
+  ${(props) =>
+    props.circleSize &&
+    css`
+      height: ${props.circleSize}px;
+      width: ${props.circleSize}px;
+    `}
 `;
 
-export const InnerContainer = styled.div`
+export const InnerContainer = styled.div<ICircleProps>`
   background: transparent;
   border: 4px solid #17e4e1;
   border-radius: 50%;
@@ -23,4 +31,18 @@ export const InnerContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  ${(props) =>
+    props.circleSize &&
+    css`
+      border-width: ${props.circleSize / 18}px;
+    `}
+
+  h2 {
+    ${(props) =>
+      props.fontSize &&
+      css`
+      font-size: ${props.fontSize}px;
+    `}
+  }
 `;
