087631279aca635542152ef95b6760fd9911f128
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2020-08-30 23:28:56 -0300

    passing parameters to button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,9 +2,9 @@ import React, { ButtonHTMLAttributes } from "react";
 
 import { Container } from "./styles";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const Button: React.FC<ButtonProps> = ({ children, ...restProps }) => {
+const Button: React.FC<IButtonProps> = ({ children, ...restProps }) => {
   return (
     <Container type="button" {...restProps}>
       {children}
